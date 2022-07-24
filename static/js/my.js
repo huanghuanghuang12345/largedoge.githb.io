1e153ae8d4972d03e55e2763a744b34f2795ca5c
weareone <98588746+huanghuanghuang12345@users.noreply.github.com>
2022-07-24 17:11:23 +0800

    Add files via upload

---
+++ static/js/my.js
@@ -0,0 +1,23 @@
+$(function(){
+$(window).scroll(function(){
+	  var top=$(window).scrollTop();
+	  if(top>30){
+		$('.head').addClass('active')
+	  }else{
+		$('.head').removeClass('active')
+	  };
+	  
+	});
+
+$(".mobile").click(function(){
+	$(this).toggleClass('active');
+	$(".m-menu").toggleClass('active');
+});	
+
+$(".m-menu span").click(function(){
+	$(".mobile").removeClass('active');
+	$(".m-menu").removeClass('active');
+});	
+
+
+})
